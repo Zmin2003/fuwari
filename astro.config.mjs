edec36075d3ea38071e901ecaf35640c9507c308
Zmin <104510233+Zmin2003@users.noreply.github.com>
2025-01-31 21:50:17 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://www.zmin.fyi",
   base: "/",
   trailingSlash: "always",
   integrations: [
